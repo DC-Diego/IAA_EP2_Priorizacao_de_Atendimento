10c84265c3d74aefbf752f13c58e33c6b06d10fb
DC-Diego <dicavallaro.dc@gmail.com>
2025-11-14 16:03:23 -0300

    Abrir arquivos, conversão da ficha string para numeros/id de ficha e transformação de ficha de pacientes

--- 16984724.c
+++ 16984724.c
@@ -1,9 +1,143 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define POTENCIA 1000000
+
+char *cores[5] = {"Azul","Verde","Amarelo","Laranja","Vermelho"};
+char *preferencias[6] = {"N/A","Idoso", "PCD", "Gravida", "Pessoa Obesa","Recem Nascido"};
+
+
+
+typedef struct {
+  int cor;
+  int preferencia;
+  int tempo;
+
+} ficha_Paciente;
+
+int matchingNumber(char * s, int coluna);
+
+int qtdLinhas(char * nomeArq);
+
+int abrirArquivo(char * nomeArq, ficha_Paciente * fichas, double * numeros);
 
 
 int main(int argc, char *argv[]){
-  printf("Hello, world! %d", argc);
+  // printf("Hello, world! %d\n", argc);
+  int linhas = qtdLinhas(argv[1]);
+  if(linhas == -1) return -1;
+  ficha_Paciente * fichas = (ficha_Paciente*) malloc(sizeof(ficha_Paciente)*linhas);
+  double * numeros = (double*) malloc(sizeof(double)*linhas);
+  abrirArquivo(argv[1], fichas, numeros);
+
+  for (int i = 0; i < linhas; i++)
+  {
+    printf("NUM: %lf\n", numeros[i]);
+  }
+  
+  //ordenar os ids correspondentes.
+
+  // printar lista ordenada substituindo os numeros/ids pela lista real.
+
+  // montar arquivo.
+
+
+
   return 0;
 }
 
+int abrirArquivo(char * nomeArq, ficha_Paciente * fichas, double * numeros){
+
+  FILE * arquivo = fopen(nomeArq, "r");
+  int c;
+  int coluna = 0, linhaAtual = 0;
+
+  char substring[1024]; 
+  substring[0] = '\0';
+
+
+  while((c = fgetc(arquivo))!=EOF){
+    if(c >= 254) continue;
+    if (c == ',' || c == '\n') {
+        // printf("CAMPO: %s\n", substring);
+        
+        switch (coluna)
+        {
+          case 0: fichas[linhaAtual].cor = matchingNumber(substring, coluna);break;
+          case 1: fichas[linhaAtual].preferencia = matchingNumber(substring, coluna);break;
+          case 2:
+            fichas[linhaAtual].tempo = atoi(substring);
+
+            numeros[linhaAtual] = (double)(fichas[linhaAtual].cor * 10 +
+    ((fichas[linhaAtual].cor >= 4 || !fichas[linhaAtual].preferencia  ) ? 0 : 1)) +
+    ((double)fichas[linhaAtual].tempo) / POTENCIA;
+            linhaAtual++;
+            break;
+        }        
+
+
+
+        substring[0] = '\0';   
+        coluna = (coluna + 1) % 3;
+
+    } else {
+        int len = strlen(substring);
+        substring[len] = c;
+        substring[len+1] = '\0';
+    }
+
+  }
+  fichas[linhaAtual].tempo = atoi(substring);
+  numeros[linhaAtual] = (double)(fichas[linhaAtual].cor * 10 +((fichas[linhaAtual].cor >= 4 || !fichas[linhaAtual].preferencia  ) ? 0 : 1)) + ((double)fichas[linhaAtual].tempo) / POTENCIA;
+
+
+  fclose(arquivo);
+
+   return 1;
+}
+
+
+
+
+int matchingNumber(char * s, int coluna){
+  switch (coluna){
+    case 0: 
+      for (int i = 0; i < 5; i++)
+        if (strcmp(cores[i], s) == 0)
+            return i + 1;
+      break;
+
+    case 1:
+      for (int i = 0; i < 6; i++)
+        if (strcmp(preferencias[i], s) == 0)
+            return i;
+      break;
+    default: break;
+  }
+
+  return -1; 
+}
+
+
+int qtdLinhas(char * nomeArq){
+  FILE * arquivo = fopen(nomeArq, "r");
+  if(arquivo == NULL){
+    printf("\nErro ao abrir o arquivo: %s\n", nomeArq);
+    return -1;
+  }
+
+  int qtd = 0;
+  int c;
+  char lastVal = '\n';
+  while((c = fgetc(arquivo))!=EOF){
+    if((char)c=='\n')qtd++;
+
+    lastVal = (char) c;
+
+  }
+  // printf("\na %d - %d\n", (int)lastVal, (int)'\n');
+  if(lastVal!='\n')qtd++;
+  fclose(arquivo);
+  return qtd;
+
+}
