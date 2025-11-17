97eb1e2366711db29f5d7a22eea23c61b74675fc
DC-Diego <dicavallaro.dc@gmail.com>
2025-11-17 12:27:43 -0300

    Criação do arquivo e lista em ordem, EP feito 100%

--- 16984724.c
+++ 16984724.c
@@ -2,11 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 #define POTENCIA 1000000
-
 char *cores[5] = {"Azul","Verde","Amarelo","Laranja","Vermelho"};
 char *preferencias[6] = {"N/A","Idoso", "PCD", "Gravida", "Pessoa Obesa","Recem Nascido"};
-
-
 
 typedef struct {
   int cor;
@@ -23,7 +20,6 @@ int abrirArquivo(char * nomeArq, ficha_Paciente * fichas, double * numeros);
 
 double escalonar(ficha_Paciente * ficha);
 
-
 void swap(int *a, int *b) {
     int temp = *a;
     *a = *b;
@@ -31,9 +27,8 @@ void swap(int *a, int *b) {
 }
 
 int partition(double nums[], int ids[], int inicio, int fim) {
-    double pivot = nums[ids[fim]];  // pivô escolhido como o último elemento
-    int i = inicio - 1;        // índice do menor elemento
-
+    double pivot = nums[ids[fim]]; 
+    int i = inicio - 1; 
     for (int j = inicio; j < fim; j++) {
         if (nums[ids[j]] <= pivot) { 
             i++;
@@ -47,17 +42,12 @@ int partition(double nums[], int ids[], int inicio, int fim) {
 void quickSort(double nums[], int ids[], int inicio, int fim) {
     if (inicio < fim) {
         int pi = partition(nums, ids, inicio, fim);
-
         quickSort(nums, ids, inicio, pi - 1);
         quickSort(nums, ids, pi + 1, fim);
     }
 }
 
-
-
-
 int main(int argc, char *argv[]){
-  // printf("Hello, world! %d\n", argc);
   int linhas = qtdLinhas(argv[1]);
   if(linhas == -1) return -1;
   ficha_Paciente * fichas = (ficha_Paciente*) malloc(sizeof(ficha_Paciente)*linhas);
@@ -66,23 +56,17 @@ int main(int argc, char *argv[]){
   for (int i = 0; i < linhas; i++) ids[i] = i;
 
   abrirArquivo(argv[1], fichas, numeros);
-
- 
-  //ordenar os ids correspondentes.
   quickSort(numeros, ids, 0, linhas-1);
-
-
-  // printar lista ordenada substituindo os numeros/ids pela lista real.
+  FILE *newFile = fopen("OrdemDeAtendimentos.csv", "w"); 
+  if (newFile == NULL) {
+      printf("Erro ao criar arquivo!\n");
+      return -1;
+  }
   for (int i = linhas-1; i >=0; i--)
   {
-    printf("%s,%s,%d\n", cores[fichas[ids[i]].cor-1], preferencias[fichas[ids[i]].preferencia], fichas[ids[i]].tempo);
+    fprintf(newFile, "%s,%s,%d\n", cores[fichas[ids[i]].cor], preferencias[fichas[ids[i]].preferencia], fichas[ids[i]].tempo);
   }
-
-
-  // montar arquivo.
-
-
-
+  fclose(newFile);
   return 0;
 }
 
@@ -90,26 +74,24 @@ double escalonar(ficha_Paciente * ficha){
 
   switch (ficha->cor)
   {
-    case 1:
+    case 0:
       ficha->cor = (ficha->tempo > 240)?ficha->cor+1:ficha->cor;
       break;
-    case 2:
+    case 1:
       ficha->cor = (ficha->tempo > 120)?ficha->cor+1:ficha->cor;
       break;
-    case 3:
+    case 2:
       ficha->cor = (ficha->tempo > 60)?ficha->cor+1:ficha->cor;
       break;
-    case 4:
+    case 3:
       ficha->cor = (ficha->tempo > 10)?ficha->cor+1:ficha->cor;
       break;
     default:
       break;
   }
 
-
-
   return (double)(ficha->cor * 10 +
-    ((ficha->cor >= 4 || !ficha->preferencia  ) ? 0 : 1)) +
+    ((ficha->cor >= 3 || !ficha->preferencia  ) ? 0 : 1)) +
     ((double)ficha->tempo) / POTENCIA;
 
 }
@@ -120,66 +102,41 @@ int abrirArquivo(char * nomeArq, ficha_Paciente * fichas, double * numeros){
   FILE * arquivo = fopen(nomeArq, "r");
   int c;
   int coluna = 0, linhaAtual = 0;
-
-  char substring[1024]; 
+  char substring[30]; 
   substring[0] = '\0';
-
-
   while((c = fgetc(arquivo))!=EOF){
     if(c >= 254) continue;
     if (c == ',' || c == '\n') {
-        // printf("CAMPO: %s\n", substring);
-        
         switch (coluna)
         {
           case 0: fichas[linhaAtual].cor = matchingNumber(substring, coluna);break;
           case 1: fichas[linhaAtual].preferencia = matchingNumber(substring, coluna);break;
           case 2:
             fichas[linhaAtual].tempo = atoi(substring);
-
-            // numeros[linhaAtual] = (double)(fichas[linhaAtual].cor * 10 +    ((fichas[linhaAtual].cor >= 4 || !fichas[linhaAtual].preferencia  ) ? 0 : 1)) +  ((double)fichas[linhaAtual].tempo) / POTENCIA;
-            printf("Antes %d", fichas[linhaAtual].cor);
             numeros[linhaAtual] = escalonar(&fichas[linhaAtual]);
-            printf("DEPOIS %d\n", fichas[linhaAtual].cor);
             linhaAtual++;
             break;
         }        
-
-
-
         substring[0] = '\0';   
         coluna = (coluna + 1) % 3;
-
     } else {
         int len = strlen(substring);
         substring[len] = c;
         substring[len+1] = '\0';
     }
-
   }
   fichas[linhaAtual].tempo = atoi(substring);
-
-  printf("Antes %d", fichas[linhaAtual].cor);
-            numeros[linhaAtual] = escalonar(&fichas[linhaAtual]);
-            printf("Depois  %d\n", fichas[linhaAtual].cor);
-
-  // numeros[linhaAtual] = (double)(fichas[linhaAtual].cor * 10 +((fichas[linhaAtual].cor >= 4 || !fichas[linhaAtual].preferencia  ) ? 0 : 1)) + ((double)fichas[linhaAtual].tempo) / POTENCIA;
-
-
+  numeros[linhaAtual] = escalonar(&fichas[linhaAtual]);
   fclose(arquivo);
-
-   return 1;
+  return 1;
 }
-
-
-
 
 int matchingNumber(char * s, int coluna){
   switch (coluna){
     case 0: 
       for (int i = 0; i < 5; i++)
         if (strcmp(cores[i], s) == 0)
-            return i + 1;
+            return i;
       break;
 
     case 1:
@@ -189,10 +146,8 @@ int matchingNumber(char * s, int coluna){
       break;
     default: break;
   }
-
   return -1; 
 }
-
 
 int qtdLinhas(char * nomeArq){
   FILE * arquivo = fopen(nomeArq, "r");
@@ -206,13 +161,9 @@ int qtdLinhas(char * nomeArq){
   char lastVal = '\n';
   while((c = fgetc(arquivo))!=EOF){
     if((char)c=='\n')qtd++;
-
     lastVal = (char) c;
-
   }
-  // printf("\na %d - %d\n", (int)lastVal, (int)'\n');
   if(lastVal!='\n')qtd++;
   fclose(arquivo);
   return qtd;
-
 }
